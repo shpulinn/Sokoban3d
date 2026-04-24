b4735ad3e416fe82300df4851d0713e358b1a84e
Nikita Shpulin <shpulinn@mail.ru>
2026-04-24 10:04:27 +0200

    Update LevelData.cs

--- Assets/_Project/Data/LevelData.cs
+++ Assets/_Project/Data/LevelData.cs
@@ -1,37 +1,34 @@
 ﻿using _Project.Core;
 using UnityEngine;
 
-namespace _Project.Data
+[CreateAssetMenu(fileName = "Level_01", menuName = "Sokoban/Level Data")]
+public class LevelData : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "Level_01", menuName = "Sokoban/Level Data")]
-    public class LevelData : ScriptableObject
+    public string LevelName;
+    public int Width;
+    public int Height;
+    public CellType[] Cells; // row-major: index = Y * Width + X
+
+    public CellType GetCell(int x, int y) => Cells[y * Width + x];
+
+    public GridPosition FindPlayer()
     {
-        public string LevelName;
-        public int Width;
-        public int Height;
-        public CellType[] Cells; // row-major: index = Y * Width + X
+        for (int y = 0; y < Height; y++)
+        for (int x = 0; x < Width; x++)
+            if (GetCell(x, y) == CellType.Player || 
+                GetCell(x, y) == CellType.PlayerOnTarget)
+                return new GridPosition(x, y);
 
-        public CellType GetCell(int x, int y) => Cells[y * Width + x];
+        throw new System.Exception($"[LevelData] No player found in {name}");
+    }
 
-        public GridPosition FindPlayer()
-        {
-            for (int y = 0; y < Height; y++)
-            for (int x = 0; x < Width; x++)
-                if (GetCell(x, y) == CellType.Player || 
-                    GetCell(x, y) == CellType.PlayerOnTarget)
-                    return new GridPosition(x, y);
+    public LevelModel ToLevelModel()
+    {
+        var grid = new CellType[Width, Height];
+        for (int y = 0; y < Height; y++)
+        for (int x = 0; x < Width; x++)
+            grid[x, y] = GetCell(x, y);
 
-            throw new System.Exception($"[LevelData] No player found in {name}");
-        }
-
-        public LevelModel ToLevelModel()
-        {
-            var grid = new CellType[Width, Height];
-            for (int y = 0; y < Height; y++)
-            for (int x = 0; x < Width; x++)
-                grid[x, y] = GetCell(x, y);
-
-            return new LevelModel(Width, Height, grid, FindPlayer());
-        }
+        return new LevelModel(Width, Height, grid, FindPlayer());
     }
 }
