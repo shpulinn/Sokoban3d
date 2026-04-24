f81f119f436133b88e6824812b46fc36bb72cf9c
Nikita Shpulin <shpulinn@mail.ru>
2026-04-24 10:03:13 +0200

    PlayerView created

---
+++ Assets/_Project/Gameplay/Views/PlayerView.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    [SerializeField] private float _moveDuration = 0.15f;
+
+    public UniTask MoveTo(Vector3 targetPos, CancellationToken ct)
+    {
+        var utcs = new UniTaskCompletionSource();
+
+        transform
+            .DOMove(targetPos, _moveDuration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() => utcs.TrySetResult())
+            .OnKill(() => utcs.TrySetResult());
+
+        ct.Register(() =>
+        {
+            transform.DOKill();
+            utcs.TrySetResult();
+        });
+
+        return utcs.Task;
+    }
+}
