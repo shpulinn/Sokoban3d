954317b3987130648f6f71554df6255b8a993abc
Nikita Shpulin <shpulinn@mail.ru>
2026-04-24 19:36:50 +0200

    LevelRepositoryBase created

---
+++ Assets/_Project/Core/Interfaces/LevelRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project.Core.Interfaces
+{
+    public abstract class LevelRepositoryBase : MonoBehaviour
+    {
+        public abstract LevelModel GetLevel(int index);
+        public abstract int GetLevelCount();
+    }
+}
