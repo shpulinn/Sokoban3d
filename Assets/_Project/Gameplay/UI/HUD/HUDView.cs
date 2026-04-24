5422f0de9e221da9d007c058befbf677b4a1d270
Nikita Shpulin <shpulinn@mail.ru>
2026-04-24 13:40:04 +0200

    HUDView created

---
+++ Assets/_Project/Gameplay/UI/HUD/HUDView.cs
@@ -0,0 +1,47 @@
+using _Project.Core.Commands;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUDView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _movesText;
+    [SerializeField] private Button _undoButton;
+    [SerializeField] private Button _restartButton;
+
+    public void Init(CommandHistory history)
+    {
+        UpdateMoves(history.MoveCount);
+        history.OnHistoryChanged += () => UpdateMoves(history.MoveCount);
+
+        _undoButton.onClick.AddListener(() => history.Undo());
+        _restartButton.onClick.RemoveAllListeners();
+    }
+
+    public void SetRestartAction(System.Action onRestart)
+    {
+        _restartButton.onClick.AddListener(() => onRestart?.Invoke());
+    }
+
+    public void SetInteractable(bool interactable)
+    {
+        _undoButton.interactable = interactable && true;
+        _restartButton.interactable = interactable;
+    }
+
+    public void UpdateUndoButton(bool canUndo)
+    {
+        _undoButton.interactable = canUndo;
+    }
+
+    private void UpdateMoves(int count)
+    {
+        _movesText.text = $"Moves: {count}";
+    }
+
+    private void OnDestroy()
+    {
+        _undoButton.onClick.RemoveAllListeners();
+        _restartButton.onClick.RemoveAllListeners();
+    }
+}
